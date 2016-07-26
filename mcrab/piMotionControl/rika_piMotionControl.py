9825294dfa02d0877961d118f8409c8013c383a3
balllee <bowie36015@gmail.com>
2016-07-26 16:36:26 +0800

    ver_0.01

--- mcrab/piMotionControl/rika_piMotionControl.py
+++ mcrab/piMotionControl/rika_piMotionControl.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+# pinNumber
 servoSingalPin = 7
 stepSingalPin = 11
 stepDirPin = 12
@@ -15,11 +16,13 @@ GPIO.setup(stepSingalPin, GPIO.OUT)
 GPIO.setup(stepDirPin, GPIO.OUT)
 GPIO.setup(stepSleepPin, GPIO.OUT)
 GPIO.setup(limitSwitchPin, GPIO.IN)
-GPIO.add_event_detect(limitSwitchPin, GPIO.FALLING, callback=limitSwitch, bouncetime=200)
+GPIO.add_event_detect(limitSwitchPin, GPIO.FALLING, callback=limitSwitch, bouncetime=100)
 GPIO.setup(irlightPin, GPIO.OUT)
 
-def limitSwitch(channel):  #limitSwitchStart
-    print 'Hello'
+# Variable
+alpha = 0
+beta = 0
+irlight = 2
 
 while True:
 	try:
@@ -27,3 +30,51 @@ while True:
 	except KeyboardInterrupt:
 		GPIO.cleanup()
 GPIO.cleanup()
+
+def rx(rxBuff):
+	buff[] = rxBuff.split()
+	for i in range(0,10,2):
+		if buff[i] == a_rel:
+			alpha += float(buff[i + 1])
+			if alpha < 0:
+				alpha += 360
+			elif alpha > 360
+				alpha -= 360
+		elif buff[i] == a_abs:
+			alpha = float(buff[i + 1])
+		elif buff[i] == b_rel:
+			beta += float(buff[i + 1])
+			if beta < 0:
+				beta += 360
+			elif beta > 360
+				beta -= 360
+		elif buff[i] == b_abs:
+			beta = float(buff[i + 1])
+		elif buff[i] == irlight:
+			if buff[i + 1] == 0:
+				pass
+			elif buff[i + 1] == 1:
+				pass
+		elif buff[i] == cal:
+			pass
+
+def tx():
+
+def motorCompute():
+	
+
+# limitSwitchStart
+def limitSwitch(channel):  
+    print 'Hello'
+
+
+'''
+def saveMethod(self):
+    if self.e == None:
+        self.displayText["text"] = "No Encrypt object can save!!"
+    else:
+        f = open('./code.txt', 'w')
+        f.write(self.e.getCode())
+        f.closed
+        self.displayText["text"] = "The code is saved."
+'''
