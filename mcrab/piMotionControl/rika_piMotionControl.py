f000af33045b21cc18006c491e766349b523ac62
balllee <bowie36015@gmail.com>
2016-07-27 14:56:39 +0800

    ver 1.00
    code complete but not test on raspi yet.

--- mcrab/piMotionControl/rika_piMotionControl.py
+++ mcrab/piMotionControl/rika_piMotionControl.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 # pinNumber
-servoSingalPin = 7
 stepSingalPin = 11
 stepDirPin = 12
 stepSleepPin = 13
@@ -11,7 +11,6 @@ irlightPin = 16
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(servoSingalPin, GPIO.OUT)
 GPIO.setup(stepSingalPin, GPIO.OUT)
 GPIO.setup(stepDirPin, GPIO.OUT)
 GPIO.setup(stepSleepPin, GPIO.OUT)
@@ -20,61 +19,71 @@ GPIO.add_event_detect(limitSwitchPin, GPIO.FALLING, callback=limitSwitch, bounce
 GPIO.setup(irlightPin, GPIO.OUT)
 
 # Variable
-alpha = 0
-beta = 0
-irlight = 2
+alpha = [180, 0]	# [rightNowAngle, leftAngle]
+irlight = 2			# 0=off 1=on 2=auto
 
-while True:
-	try:
+# alphaMotorVariable
+stepAngle = 0.45
+stepSpeed = 0.0008	# us
+# stepGeer = 0
+threadSwitch = 0
 
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-GPIO.cleanup()
+# motorThread
+motorThread = threading.Thread(target=motorTd,name='motorThread')
 
-def rx(rxBuff):
+def motorTd(alphaStep):
+	GPIO.output(stepSleepPin, GPIO.HIGH)
+	while alphaStep >= 0 && threadSwitch >= 1:
+		GPIO.output(stepSingalPin, GPIO.LOW)
+		time.sleep(stepSpeed)
+		GPIO.output(stepSingalPin, GPIO.HIGH)
+		time.sleep(stepSpeed)
+		alpha[0] += stepAngle
+		alphaStep -= 1
+	GPIO.output(stepSleepPin, GPIO.LOW)
+
+def receive(rxBuff):
 	buff[] = rxBuff.split()
-	for i in range(0,10,2):
-		if buff[i] == a_rel:
-			alpha += float(buff[i + 1])
-			if alpha < 0:
-				alpha += 360
-			elif alpha > 360
-				alpha -= 360
-		elif buff[i] == a_abs:
-			alpha = float(buff[i + 1])
-		elif buff[i] == b_rel:
-			beta += float(buff[i + 1])
-			if beta < 0:
-				beta += 360
-			elif beta > 360
-				beta -= 360
-		elif buff[i] == b_abs:
-			beta = float(buff[i + 1])
-		elif buff[i] == irlight:
-			if buff[i + 1] == 0:
-				pass
-			elif buff[i + 1] == 1:
-				pass
-		elif buff[i] == cal:
-			pass
+	for i in range(0,len(buff),2):
+		if threadSwitch != 2
+			if buff[i] == a_rel:	# relative
+				threadSwitch = 0
+				alphaBuff = float(buff[i + 1])
+				motorCompute(alphaBuff, 1)
+			elif buff[i] == a_abs:	# absolut
+				threadSwitch = 0
+				alphaBuff = float(buff[i + 1]) - alpha[0]
+				motorCompute(alphaBuff, 1)
+			elif buff[i] == irlight:
+				irlight = buff[i + 1]
+			elif buff[i] == cal:
+				threadSwitch = 0
+				alpha[0] = 360
+				motorCompute(-360, 2)
 
-def tx():
+def transmit():
 
-def motorCompute():
-	
+def motorCompute(alphaBuff, TdBuff):
+	if alphaBuff + alpha[0] < 0:
+		alphaBuff += 360
+	elif alphaBuff + alpha[0] > 360
+		alphaBuff -= 360
+
+	if(alphaBuff > 0):
+		GPIO.output(stepDirlPin, GPIO.HIGH)
+	else:
+		GPIO.output(stepDirlPin, GPIO.LOW)
+	time.sleep(0.0017)
+	threadSwitch = TdBuff
+	motorThread.start(abs(int(alphaBuff/stepAngle)))
+
 
 # limitSwitchStart
 def limitSwitch(channel):  
-    print 'Hello'
-
-
-'''
-def saveMethod(self):
-    if self.e == None:
-        self.displayText["text"] = "No Encrypt object can save!!"
-    else:
-        f = open('./code.txt', 'w')
-        f.write(self.e.getCode())
-        f.closed
-        self.displayText["text"] = "The code is saved."
-'''
+    threadSwitch = 0
+    while GPIO.input(limitSwitchPin) == GPIO.LOW:
+    	motorCompute(1, 2)
+    	time.sleep(0.01)
+    alpha[0] = 0
+    alpha[1] = 0
+    threadSwitch = 0
